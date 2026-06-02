29f003125422d4518974ca98eecf1b3653fa6fe1
aresnasa <aresnasa@126.com>
2026-06-02 23:34:14 +0800

    bump iMonitor to v0.3.0

--- Formula/iMonitor.rb
+++ Formula/iMonitor.rb
@@ -2,7 +2,7 @@ class Imonitor < Formula
   desc "macOS menu bar system monitor - CPU, Memory, GPU, Network"
   homepage "https://github.com/aresnasa/iMonitor"
   url "https://github.com/aresnasa/iMonitor/releases/download/v0.3.0/iMonitor-v0.3.0.zip"
-  sha256 "901804ba84b1766a1c902df8fd922dd810cf8abe6a3addeca6100a98f88df365"
+  sha256 "59105a2b302613688e7b7ec177c3a291390c166ce6a643169196f4f7d8b70c7e"
   version "0.3.0"
 
   depends_on :macos => :big_sur
